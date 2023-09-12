3b5d064201dad101d6ce913aa46181af6e45facc
lisssachka <114672738+lisssachka@users.noreply.github.com>
2023-09-12 16:33:01 +0400

    Added Homework 3

---
+++ homework3/Task1.java
@@ -0,0 +1,149 @@
+package homework3;
+import java.util.*;
+
+public class Task1 {
+    static String[] board;
+    static String turn;
+
+    static String checkWinner()
+    {
+        for (int a = 0; a < 8; a++) {
+            String line = null;
+
+            switch (a) {
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] + board[6];
+                    break;
+                case 4:
+                    line = board[1] + board[4] + board[7];
+                    break;
+                case 5:
+                    line = board[2] + board[5] + board[8];
+                    break;
+                case 6:
+                    line = board[0] + board[4] + board[8];
+                    break;
+                case 7:
+                    line = board[2] + board[4] + board[6];
+                    break;
+            }
+            if (line.equals("XXX")) {
+                return "X";
+            }
+
+            else if (line.equals("OOO")) {
+                return "O";
+            }
+        }
+
+        for (int a = 0; a < 9; a++) {
+            if (Arrays.asList(board).contains(
+                    String.valueOf(a + 1))) {
+                break;
+            }
+            else if (a == 8) {
+                return "draw";
+            }
+        }
+
+
+        System.out.println(turn + " ходит. введите номер ячейки для "
+                        + turn
+        );
+        return null;
+    }
+
+    static void printBoard()
+    {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                + board[1] + " | " + board[2]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                + board[4] + " | " + board[5]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                + board[7] + " | " + board[8]
+                + " |");
+        System.out.println("|---|---|---|");
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        board = new String[9];
+        turn = "X";
+        String winner = null;
+
+        for (int a = 0; a < 9; a++) {
+            board[a] = String.valueOf(a + 1);
+        }
+
+        System.out.println("Добро пожаловать в крестики-нолики 3х3.");
+        printBoard();
+
+        System.out.println(
+                "X ходит первым. Введите номер ячейки для X ");
+
+        while (winner == null) {
+            int numInput;
+
+            try {
+                numInput = in.nextInt();
+                if (!(numInput > 0 && numInput <= 9)) {
+                    System.out.println(
+                            "Ошибка! Выберите ячейку 1-9! ");
+                    continue;
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println(
+                        "Ошибка! Выберите ячейку 1-9! ");
+                continue;
+            }
+
+            if (board[numInput - 1].equals(
+                    String.valueOf(numInput))) {
+                board[numInput - 1] = turn;
+
+                if (turn.equals("X")) {
+                    turn = "O";
+                }
+                else {
+                    turn = "X";
+                }
+
+                printBoard();
+                winner = checkWinner();
+            }
+            else {
+                System.out.println(
+                        "Ячейка уже занята. Выберите свободную ");
+            }
+        }
+
+        if (winner.equalsIgnoreCase("draw")) {
+            System.out.println(
+                    "Ничья! Поле заполнено. Спасибо за игру.");
+        }
+
+        // For winner -to display Congratulations! message.
+        else {
+            System.out.println(
+                    "Поздравляем! " + winner
+                            + " выиграл! Спасибо за игру.");
+        }
+        in.close();
+    }
+}
